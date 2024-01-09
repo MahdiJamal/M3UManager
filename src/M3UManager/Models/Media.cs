295885e26b2736f46855a843f404759ac87d6b79
MahdiJamal <mahdiiijamal@gmail.com>
2024-01-09 14:25:14 +0330

    Changed c# language to latest version.

--- src/M3UManager/Models/Media.cs
+++ src/M3UManager/Models/Media.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 
-namespace M3UManager.Models
+namespace M3UManager.Models;
+
+public class Media : ICloneable
 {
-    public class Media : ICloneable
+    public Uri StreamUri { get; set; }
+    public ExtinfTag ExtinfTag { get; set; }
+
+    public object Clone()
+        => MemberwiseClone();
+    public T Clone<T>()
+        => (T)Clone();
+
+    public void Reset()
     {
-        public Uri StreamUri { get; set; }
-        public ExtinfTag ExtinfTag { get; set; }
-
-        public object Clone()
-            => MemberwiseClone();
-        public T Clone<T>()
-            => (T)Clone();
-
-        public void Reset()
-        {
-            StreamUri = null;
-            ExtinfTag = null;
-        }
+        StreamUri = null;
+        ExtinfTag = null;
     }
 }
--- src/M3UManager/Utilities/StringUtility.cs
+++ src/M3UManager/Utilities/StringUtility.cs
@@ -6,5 +6,9 @@ namespace M3UManager.Utilities
     {
         internal static string[] Split(this string text, string seprator, StringSplitOptions options = StringSplitOptions.None)
             => text.Split(new string[] { seprator }, options);
+        internal static string[] Split(this string text, params string[] seprators)
+            => text.Split(seprators, StringSplitOptions.None);
+        internal static string[] Split(this string text, StringSplitOptions options, params string[] seprators)
+            => text.Split(seprators, options);
     }
 }
